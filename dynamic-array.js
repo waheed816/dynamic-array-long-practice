2d97b35cb45674fb8c316a69583457d90e588799
Mehedi Meem <waheed816@gmail.com>
2022-10-25 17:32:41 -0400

    almost done

--- dynamic-array.js
+++ dynamic-array.js
@@ -2,43 +2,63 @@ class DynamicArray {
 
   constructor(defaultSize=4) {
 
-    // Your code here
+    this.capacity = defaultSize;
+    this.length = 0;
+    this.data = new Array(this.capacity);
+
   }
 
   read(index) {
-
-    // Your code here
+    if(this.length <= index){
+      return undefined;
+    }
+    return this.data[index];
   }
 
   push(val) {
-
-    // Your code here
+    this.data[this.length] = val;
+    this.length++;
   }
 
-
   pop() {
-
-    // Your code here
+    let element = this.data[this.length - 1];
+    this.length--;
+    return element;
   }
 
   shift() {
-
-    // Your code here
+    let element = this.data[0];
+    for(let i = 0; i < this.length ; i++){
+      this.data[i] = this.data[i+1];
+    }
+    this.length--;
+    return element;
   }
 
   unshift(val) {
-
-    // Your code here
+    for(let i = this.length; i > 0 ; i--){
+      this.data[i] = this.data[i-1];
+    }
+    this.data[0] = val;
+    this.length++;
   }
 
   indexOf (val) {
-
-    // Your code here
+    for(let i = 0; i <= this.length -1; i++){
+      if(val === this.data[i]){
+        return i;
+      }
+    }
+    return -1;
   }
 
   resize () {
-
-    // Your code here
+    //this.capacity *= 2;
+    for(let i = 0; i < this.length ; i++){
+      
+      this.data[i+this.length] = this.data[i];
+    }
+    //this.data.length *= 2;
   }
 
 }
